783842759121b47674c6a52d2c35588847434919
Jairo Navez <jairowolf123@hotmail.com>
2019-06-22 12:40:43 -0500

    2

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,39 @@ namespace WpfApp1
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			// https://es.ourcodeworld.com/articulos/leer/70/como-conectarse-a-una-base-de-datos-mysql-con-c-en-winforms-y-xampp
+			string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=prueba;";
+			string query = "SELECT * FROM tabla";
+			MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+			MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+			commandDatabase.CommandTimeout = 60;
+			MySqlDataReader reader;
+			try
+			{
+				databaseConnection.Open();
+				reader = commandDatabase.ExecuteReader();
+				if (reader.HasRows)
+				{
+					while (reader.Read())
+					{
+						string[] row = { reader.GetInt32(0).ToString(), reader.GetString(1) };
+						MessageBox.Show(row[1]);
+					}
+				}
+				else
+				{
+					Console.WriteLine("No se encontraron datos.");
+				}
+				databaseConnection.Close();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
 		}
 	}
 }
