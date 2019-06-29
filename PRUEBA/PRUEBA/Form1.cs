fec7cdcd21df5f932cb8bd447a2c917d52bc09f0
Jairo Navez <jairowolf123@hotmail.com>
2019-06-29 01:55:26 -0500

    3

---
+++ PRUEBA/PRUEBA/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PRUEBA
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int x1, x2;
+			x1 = Convert.ToInt32(textBox1.Text);
+			x2 = Convert.ToInt32(textBox2.Text);
+			MessageBox.Show((x1 + x2).ToString());
+		}
+	}
+}
